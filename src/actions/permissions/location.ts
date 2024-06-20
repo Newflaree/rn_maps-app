2dce686968a5fd2b33ca0a935d677b4108497ae4
Camilo López <camiloj.lopez79@gmail.com>
2024-06-20 18:44:27 -0400

    Revisar permiso de ubicación

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -2,6 +2,7 @@
 import { Platform } from 'react-native';
 // React Native Permissions
 import {
+  check,
   PERMISSIONS,
   PermissionStatus as RNPermissionStatus,
   openSettings,
@@ -25,6 +26,28 @@ export const requestLocationPermission = async (): Promise<PermissionStatus> => 
   if ( status === 'blocked' ) {
     await openSettings();
     // TODO: return await  checkLocationPermission();
+  }
+
+  const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
+    granted: 'granted',
+    denied: 'denied',
+    blocked: 'blocked',
+    unavailable: 'unavailable',
+    limited: 'limited'
+  }
+
+  return permissionMapper[status] ?? 'unavailable';
+}
+
+export const checkLocationPermission = async (): Promise<PermissionStatus> => {
+  let status: RNPermissionStatus = 'unavailable';
+
+  if ( Platform.OS === 'ios' ) {
+    status = await check( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE );
+  } else if ( Platform.OS === 'android' ) {
+    status = await check( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION );
+  } else {
+    throw new Error( 'Unsupported platform' );
   }
 
   const permissionMapper: Record<RNPermissionStatus, PermissionStatus> = {
